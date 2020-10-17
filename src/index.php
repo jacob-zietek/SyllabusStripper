9a5f02000ab65893c3305f65f0383c9707768515
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2020-10-17 14:41:41 -0400

    very good inspiration yes yes now that is what I call a gamer moment

--- src/index.php
+++ src/index.php
@@ -1,14 +1,30 @@
 <?php
-if(isset($_POST['submit'])){
-    $name       = $_FILES['file']['name'];
-    $temp_name  = $_FILES['file']['tmp_name'];
-    if(isset($name) and !empty($name)){
-        $location = '..';
-        if(move_uploaded_file($temp_name, $location.$name)){
-            echo 'File uploaded successfully';
+if ( isset( $_FILES['pdfFile'] ) ) {
+    if ($_FILES['pdfFile']['type'] == "application/pdf" || $_FILES['pdfFile']['type'] == "application/msword" || $_FILES['pdfFile']['type'] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+        $source_file = $_FILES['pdfFile']['tmp_name'];
+        $dest_file = "../backend/userfile";
+
+        if (file_exists($dest_file)) {
+            print "The file name already exists!!";
         }
-    } else {
-        echo 'You should select a file to upload !!';
+        else {
+            move_uploaded_file( $source_file, $dest_file )
+            or die ("Error!!");
+            if($_FILES['pdfFile']['error'] == 0) {
+                print "PDF file uploaded successfully!";
+                print "<b><u>Details : </u></b><br/>";
+                print "File Name : ".$_FILES['pdfFile']['name']."<br.>"."<br/>";
+                print "File Size : ".$_FILES['pdfFile']['size']." bytes"."<br/>";
+                print "File location : upload/".$_FILES['pdfFile']['name']."<br/>";
+            }
+        }
+    }
+    else {
+        if ( $_FILES['pdfFile']['type'] != "application/pdf") {
+            print "Error occured while uploading file : ".$_FILES['pdfFile']['name']."<br/>";
+            print "Invalid  file extension, should be pdf !!"."<br/>";
+            print "Error Code : ".$_FILES['pdfFile']['error']."<br/>";
+        }
     }
 }
 
