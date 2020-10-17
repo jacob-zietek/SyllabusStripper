5a4eb0dd7e585cfa68598eaa081303d2d94ce9f7
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2020-10-17 12:41:35 -0400

    possible file integration, downloads file to backend/

--- src/index.php
+++ src/index.php
@@ -1,2 +1,9 @@
 <?php
+$targetfolder = "backend/";
+if (move_uploaded_file($_FILES['file']['upload'], $targetfolder)) {
+    echo "The file ". basename( $_FILES['file']['name']). " is uploaded";
+} else {
+    echo "Problem uploading file";
+}
+
 
