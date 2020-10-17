3aabf6612fc4f296ee370f937f2af2051e5c365b
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2020-10-17 13:59:55 -0400

    still working on file integration, update to README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SyllabusStripper
+For HelloWorld 2020, our group decided we wanted to work on a full-stack website that utilizes machine learning in order to scrape an uploaded syllabus and return a .ics file containing all the important due-dates for a college student. We're really excited to participate and give you guys an amazing product!
+
+SyllabusStripper 0.0.1
+
+Functions and Features
+
+- Custom website
+- Integrated machine learning model for scraping syllabi
+- 
+
+Bug Fixes
+
+- 
+- 
+- 
--- src/index.php
+++ src/index.php
@@ -1,9 +1,15 @@
 <?php
-$targetfolder = "backend/";
-if (move_uploaded_file($_FILES['file']['upload'], $targetfolder)) {
-    echo "The file ". basename( $_FILES['file']['name']). " is uploaded";
-} else {
-    echo "Problem uploading file";
+if(isset($_POST['submit'])){
+    $name       = $_FILES['file']['name'];
+    $temp_name  = $_FILES['file']['tmp_name'];
+    if(isset($name) and !empty($name)){
+        $location = '..';
+        if(move_uploaded_file($temp_name, $location.$name)){
+            echo 'File uploaded successfully';
+        }
+    } else {
+        echo 'You should select a file to upload !!';
+    }
 }
 
 
