a335d2bc6e8bfcff59ada029f12d76cbae3ba4b1
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2020-10-17 23:03:49 -0400

    add compatibility between frontend and backend utilizing flask

--- backend/sever.py
+++ backend/sever.py
@@ -9,10 +9,10 @@ def hello_world():
     return 'Hello, World!'
     
 @app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save('uploaded_file.pdf')
+  def upload_file():
+     if request.method == 'POST':
+         f = request.files['file']
+         f.save('uploaded_file.pdf')
 
 if __name__ == "__main__":
     app.run(debug=True)
